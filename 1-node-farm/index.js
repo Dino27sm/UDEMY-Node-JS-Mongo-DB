5c253022b14bc38c0dbeae15920b56d83b77c42d
Mironov <mironov5712s@gmail.com>
2024-10-20 17:51:55 +0300

    Update index.js

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const http = require("http");
 
+////// ============= FILES Read / Write ============================
 // // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -8,21 +10,30 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output-1.txt", textOut);
 // console.log("File has been written!");
 
-// Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      fs.writeFile(
-        "./txt/final.txt",
-        `${data2} --- ${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("The final file has been written ! 😁");
-        }
-      );
-    });
-  });
+// // Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2} --- ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("The final file has been written ! 😁");
+//         }
+//       );
+//     });
+//   });
+// });
+
+// console.log("----------- Start reading files ! --------------");
+////
+////// ============= SERVER ============================
+const server = http.createServer((req, res) => {
+  res.end("Hello from the Server port 8070 !");
 });
 
-console.log("----------- Start reading files ! --------------");
+server.listen("8070", "127.0.0.1", () => {
+  console.log("Listening to requests from Server !");
+});
