a84fe92ea7ada7b2d08011989a22c0be72b4fc09
Mironov <mironov5712s@gmail.com>
2024-11-18 17:40:25 +0200

    Update event-loop.js

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 setTimeout(() => console.log('Timer 1 finished !'), 0);
 setImmediate(() => console.log('Immediate 1 finished !'));
 
-fs.readFile('test-file.txt', () => {
+fs.readFile('test-file.txt', 'utf8', (err, data) => {
   console.log('------------- I/O finished ! -------------');
 
   setTimeout(() => console.log('Timer 2 finished !'), 0);
