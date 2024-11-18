ea218a0e671f610d480f46bb5eba8e9005673227
Mironov <mironov5712s@gmail.com>
2024-11-18 19:23:06 +0200

    Update events.js

--- 2-how-node-works/events.js
+++ 2-how-node-works/events.js
@@ -1,6 +1,11 @@
 const EventEmitter = require('events');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmitter = new Sales();
 
 myEmitter.on('newSale', () => {
   console.log('There was a new sale!');
@@ -10,4 +15,8 @@ myEmitter.on('newSale', () => {
   console.log('Customer name: Dino');
 });
 
-myEmitter.emit('newSale');
+myEmitter.on('newSale', (stock) => {
+  console.log(`There now ${stock} items left in stock`);
+});
+
+myEmitter.emit('newSale', 9);
