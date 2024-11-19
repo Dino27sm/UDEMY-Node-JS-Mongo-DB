b3f3b83f6b25ad4d4cc757d6021f0c651cd07009
Mironov <mironov5712s@gmail.com>
2024-11-19 10:03:41 +0200

    Update streams.js

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -2,14 +2,24 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-  // Solution 1 - Read entire file
-  fs.readFile('test-fille.txt', 'utf8', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // // Solution 1 - Read entire file
+  // fs.readFile('test-fille.txt', 'utf8', (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // Solution 2 - Streams (reding data file piece by piece)
+  const readable = fs.createReadStream('test-file.txt');
+  readable.on('data', (chunk) => {
+    res.write(chunk);
+  });
+  readable.on('end', () => {
+    res.end();
   });
 });
-
+//
 //---------------- START Server ------------------
+//
 server.listen(8070, '127.0.0.1', () => {
   console.log('Listening ...');
 });
