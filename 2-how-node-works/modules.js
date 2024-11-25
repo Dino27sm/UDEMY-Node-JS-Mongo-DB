ebdd2b852155d7ccd14c334a5caf437e94450248
Mironov <mironov5712s@gmail.com>
2024-11-25 11:21:19 +0200

    Update modules.js

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -1,7 +1,14 @@
 // console.log(arguments);
 // console.log(require('module').wrapper);
 
+// Using "module.exports"
 const C = require('./test-module-1.js');
 const calc1 = new C();
 
 console.log(calc1.add(2, 5));
+
+// Using just "exports"
+const calc2 = require('./test-module-2.js');
+
+console.log(calc2.add(6, 7));
+console.log(calc2.multiply(4, 5));
