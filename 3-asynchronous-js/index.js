5aac18bc0d9896746bdf4b0b5b55ccbe74189b05
Mironov <mironov5712s@gmail.com>
2024-11-27 08:53:21 +0200

    Promise used

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -3,14 +3,21 @@ const fs = require('fs');
 // To use "superagent" function from "npm" packages
 const superagent = require('superagent');
 
-fs.readFile('dog.txt', 'utf8', (err, data) => {
-  if (err) return console.log(err.message);
-  console.log(data);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) reject('I could not find that file! 😒');
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
+  console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images`)
     .then((res) => {
-      console.log(res.body.message);
       const dogPictures = res.body.message.join(',\n');
 
       fs.writeFile('dog-img.txt', dogPictures, (err) => {
