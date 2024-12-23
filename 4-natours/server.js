2586833d3628284612437aa0b4e8d844203d348d
Mironov <mironov5712s@gmail.com>
2024-12-23 18:53:54 +0200

    Update server.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
