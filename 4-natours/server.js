17d143887814903e5e3d4871729803bf35d44464
Mironov <mironov5712s@gmail.com>
2025-01-11 20:07:32 +0200

    Update server.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,11 +1,32 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 // By this line - read the file and set the variables in it
 // to "nodeJS" environment
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
 
+const localDB = 'mongodb://localhost:27017/myDatabase';
+const DB = localDB;
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection succeeded !!!');
+  });
+
+//
 console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
