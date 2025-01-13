53e118af72b6b231277ab2925ccf6cb25ee09471
Mironov <mironov5712s@gmail.com>
2025-01-13 09:19:53 +0200

    Update tourController.js

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -5,11 +5,12 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is: ${val}`);
-  if (Number(req.params.id) > tours.length) {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid ID'
     });
   }
   next();
@@ -19,51 +20,56 @@ exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price!',
+      message: 'Missing name or price'
     });
   }
   next();
 };
 
-// ROUTE Handlers ==========================================
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
     results: tours.length,
     data: {
-      tours: tours,
-    },
+      tours
+    }
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = Number(req.params.id);
-  const tour = tours.find((elm) => elm.id === id);
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
     status: 'success',
     data: {
-      tour: tour,
-    },
+      tour
+    }
   });
 };
 
 exports.createTour = (req, res) => {
+  // console.log(req.body);
+
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
+
   fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
+    `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    err => {
       res.status(201).json({
         status: 'success',
         data: {
-          tour: newTour,
-        },
+          tour: newTour
+        }
       });
     }
   );
@@ -73,14 +79,14 @@ exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour is here ...!>',
-    },
+      tour: '<Updated tour here...>'
+    }
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: null
   });
 };
