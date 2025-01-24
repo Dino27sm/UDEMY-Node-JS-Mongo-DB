293ba7a239c68b8696f5fc31a6b75d90af6013a7
Mironov <mironov5712s@gmail.com>
2025-01-24 10:34:28 +0200

    Update tourModel.js

--- 4-1-test/models/tourModel.js
+++ 4-1-test/models/tourModel.js
@@ -4,8 +4,20 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'A tour must have a name.'],
+    required: [true, 'A tour must have a name.'],
     unique: true,
+  },
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty.'],
   },
   rating: {
     type: Number,
