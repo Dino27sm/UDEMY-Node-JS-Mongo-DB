a903a210699126c176d532c715f2e6e1d15fc307
Mironov <mironov5712s@gmail.com>
2025-02-07 07:04:50 +0200

    Update tourController.js

--- 4-1-test/controllers/tourController.js
+++ 4-1-test/controllers/tourController.js
@@ -163,6 +163,13 @@ exports.getMonthlyPlan = async (req, res) => {
           },
         },
       },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
     ]);
 
     res.status(200).json({
