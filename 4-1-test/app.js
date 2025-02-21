6bbcc3834b69c9738ec44e854fcc63427b29a067
Mironov <mironov5712s@gmail.com>
2025-02-21 12:12:15 +0200

    Update app.js

--- 4-1-test/app.js
+++ 4-1-test/app.js
@@ -37,6 +37,16 @@ app.all('*', (req, res, next) => {
   });
   next();
 });
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'ERROR !';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 //----------------------------------------------------------------------
 
 module.exports = app;
