ec66479f6a941be1df78e84a10729b27eb54de2f
Mironov <mironov5712s@gmail.com>
2025-02-26 11:38:32 +0200

    Update app.js

--- 4-1-test/app.js
+++ 4-1-test/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -31,16 +32,7 @@ app.use('/api/v1/users', userRouter);
 //----------------------------------------------------------------------
 // The following appliss if incorrect URL has been sent !!!
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cannot find ${req.originalUrl} in this server !!!`,
-  // });
-
-  const err = new Error(`Cannot find ${req.originalUrl} in this server !!!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Cannot find ${req.originalUrl} in this server !!!`, 404));
 });
 
 app.use((err, req, res, next) => {
