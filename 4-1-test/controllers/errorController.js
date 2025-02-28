fc8d163a580b52cc668c34de3a259043e80ef955
Mironov <mironov5712s@gmail.com>
2025-02-28 15:54:31 +0200

    Update errorController.js

--- 4-1-test/controllers/errorController.js
+++ 4-1-test/controllers/errorController.js
@@ -1,12 +1,60 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational Errors => Send Error Messages to Clients
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('ERROR 💥💥💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong !!!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'ERROR !';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    // message: err.message,
-    message: err,
-  });
+  let errDuplct = JSON.parse(JSON.stringify(err));
 
+  if (errDuplct.name === 'CastError') {
+    errDuplct = handleCastErrorDB(errDuplct);
+  }
+
+  sendErrorProd(errDuplct, res);
   next();
 };
+//
+// //=============== Correctly Working OLD Version ================
+// module.exports = (err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'ERROR !';
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     // message: err.message,
+//     message: err,
+//   });
+
+//   next();
+// };
