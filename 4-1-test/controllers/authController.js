67d96a57dc4e828df3156b792cd8c8764acd7a2f
Mironov <mironov5712s@gmail.com>
2025-03-21 21:17:23 +0200

    Update authController.js

--- 4-1-test/controllers/authController.js
+++ 4-1-test/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -22,3 +23,20 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1. Check if "email" and "password" exist
+  if (!email || !password) {
+    next(new AppError('Please, provide email and password !', 400));
+  }
+  // 2. Check if User exists and the "password" is correct
+
+  // 3. If everything is OK, send "jwt" token to the client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
