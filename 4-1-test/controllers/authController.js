ac78677747261fb75a46d0c473df8f957da16d93
Mironov <mironov5712s@gmail.com>
2025-04-02 11:33:07 +0300

    Update authController.js

--- 4-1-test/controllers/authController.js
+++ 4-1-test/controllers/authController.js
@@ -116,9 +116,33 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = User.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
-  // 3. -----
+  // 3. Send it to the mail of the user
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
 
-  next();
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \n If you remember your password, then ignore this email.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Your password reset token (valid for 10 min)`,
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to the email !',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('Error in sending the email ! Try again later !', 500)
+    );
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
